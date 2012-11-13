c8e4ae8822b19d5bc0cfda613980565e71fbe9a1
Dieter Pisarewski <dieter@pisarewski.info>
2012-11-13 13:34:51 +0100

    Visualisierung

---
+++ src/a01_p02_dp_bl/GraphViz.java
@@ -0,0 +1,52 @@
+package a01_p02_dp_bl;
+
+import org.jgrapht.Graph;
+import org.jgrapht.ext.DOTExporter;
+import org.jgrapht.ext.VertexNameProvider;
+import org.jgrapht.graph.DefaultWeightedEdge;
+
+import java.io.*;
+
+public class GraphViz {
+
+    public static <V, E> void buildDotFile(String filePath, Graph<V, E> graph) throws IOException {
+        DOTExporter<V, E> export =
+                new DOTExporter<V, E>(new VertexNameProvider<V>() {
+
+                    @Override
+                    public String getVertexName(V vertex) {
+                    return vertex.toString();
+                    }
+                }, new VertexNameProvider<V>() {
+
+                    @Override
+                    public String getVertexName(V vertex) {
+                        return vertex.toString();
+                    }
+                }, null);
+
+        Writer writer = new BufferedWriter(new FileWriter(filePath + ".dot"));
+        export.export(writer, graph);
+        writer.close();
+        compileDotFile(filePath);
+    }
+
+    public static void compileDotFile(String dotFileName){
+        String cmd = "dot -Tpng -o " + dotFileName + ".png "+ dotFileName +".dot";
+        try {
+            Runtime.getRuntime().exec(cmd);
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+            buildDotFile("src/files/graph1",   new GraphDirected(DefaultWeightedEdge.class).load("src/files/graph1.gka"));
+            buildDotFile("src/files/graph2",   new GraphUndirected(DefaultWeightedEdge.class).load("src/files/graph2.gka"));
+            buildDotFile("src/files/graphDK5", new GraphDirected(DefaultWeightedEdge.class).load("src/files/graphDK5.gka"));
+            buildDotFile("src/files/graphDVL", new GraphDirected(DefaultWeightedEdge.class).load("src/files/graphDVL.gka"));
+            buildDotFile("src/files/graphUK5", new GraphUndirected(DefaultWeightedEdge.class).load("src/files/graphUK5.gka"));
+            buildDotFile("src/files/graphUVL", new GraphUndirected(DefaultWeightedEdge.class).load("src/files/graphUVL.gka"));
+    }
+
+}
